c0388987c94f9acd35a9647d9e950ea3701cd476
Alyona Stepanova <kiriyuuhiyori@gmail.com>
2024-05-27 17:56:46 +0300

    set linter

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,4 +3,3 @@
 import welcomeUser from "../src/cli.js";
 
 welcomeUser();
-
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const welcomeUser = () => {
+const welcomeUser = () => { 
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${playerName}!`);
 }
 
 export default welcomeUser;
-
