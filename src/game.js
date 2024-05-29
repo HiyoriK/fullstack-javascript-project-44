fee46df6bbf402975adfa3a48521c0ec07527d45
Alyona Stepanova <kiriyuuhiyori@gmail.com>
2024-05-29 11:51:46 +0300

    fix round-counter

--- src/game.js
+++ src/game.js
@@ -1,8 +1,19 @@
+import readlineSync from 'readline-sync';
 import playerName from './cli.js';
 
-const playGame = (game, task) => {
+const playGame = (roundCounter, task) => {
+  console.log(`Hello, ${playerName}!`);
   console.log(task);
-  game();
+  for (let i = 0; i < 3; i += 1) {
+    const [question, rightAnswer] = roundCounter();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (rightAnswer !== userAnswer) {
+      console.log(`'${userAnswer}' is wrong asnwer ;(. The Correct answer is '${rightAnswer}'\nLet's try again!, ${playerName}`);
+      return;
+    }
+    console.log('Correct!');
+  }
   console.log(`Congratulations, ${playerName}!`);
 };
 
--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,7 +1,3 @@
-#!/usr/bin/env node
-
-import readlineSync from 'readline-sync';
-import playerName from '../cli.js';
 import playGame from '../game.js';
 
 const showRandomNumber = (min = 0, max = 100) => {
@@ -9,22 +5,11 @@ const showRandomNumber = (min = 0, max = 100) => {
   return result;
 };
 const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
-
 const startEven = () => {
-  for (let i = 0; i < 3; i += 1) {
-    const question = showRandomNumber(0, 100);
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    const isEven = (num) => (num % 2 === 0);
-
-    const rightAnswer = (isEven(question)) ? 'yes' : 'no';
-
-    if (rightAnswer !== userAnswer) {
-      console.log(`'${userAnswer}' is wrong asnwer ;(. The Correct answer is '${rightAnswer}'\nLet's try again!, ${playerName}`);
-    } else {
-      console.log('Correct!');
-    }
-  }
+  const isEven = (num) => (num % 2 === 0);
+  const question = showRandomNumber(0, 100);
+  const rightAnswer = (isEven(question)) ? 'yes' : 'no';
+  return [question, rightAnswer];
 };
 
 const playEvenGame = () => {
