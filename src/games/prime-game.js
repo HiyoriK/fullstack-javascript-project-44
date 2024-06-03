92e8e438dadd571effb194deeef24ea6b8ff1f0a
Alyona Stepanova <kiriyuuhiyori@gmail.com>
2024-06-03 15:56:08 +0300

    start brain-prime

---
+++ src/games/prime-game.js
@@ -0,0 +1,18 @@
+import playGame from '../game.js';
+import showRandomNumber from '../utils.js';
+
+const gameTask = 'What number is missing in the progression?';
+
+
+const startPrime = () => {
+
+//   const rightAnswer = String(progression[index - 1]);
+  
+//   return [question, rightAnswer];
+};
+
+const playPrimeGame = () => {
+  playGame(startPrime, gameTask);
+};
+
+export default playPrimeGame;
