44dbb17d29583ea867a75d04a45695279940fc72
Alyona Stepanova <kiriyuuhiyori@gmail.com>
2024-08-13 12:43:31 +0300

    progression fix

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-const getRandomInRange = (min = 0, max = 100) => Math.floor(Math.random() * (max - min + 1) + min);
+const getRandomInRange = (min = 0, max = 100) => Math.floor(Math.random() * (max - min + 1));
 
 export default getRandomInRange;
